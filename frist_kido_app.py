bc2737101b9b333f163e8e3bb648c1409fc2b2a2
kido <61651321+syskido@users.noreply.github.com>
2023-09-25 16:39:08 +0900

    Add files via upload

--- frist_kido_app.py
+++ frist_kido_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import time
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from langchain.chat_models import ChatOpenAI
-
+chat_model = ChatOpenAI()
 # from langchain.llms import OpenAI
 # llm = OpenAI()
 
@@ -12,15 +12,11 @@ from langchain.chat_models import ChatOpenAI
 # result = llm.predict(content + "에 대한 시를 써줘")
 # print(result)
 st.link_button("나령 윤기도", "https://syskido.tistory.com")
-
-chat_model = ChatOpenAI()
-
 st.text('인공지능 Kido 단편소설')
 content = st.text_input('단편소설의 주제를 제시해주세요.')
 if st.button('단편소설 2000자 내외 작성 요청하기'):
     with st.spinner('단편소설 작성 중...'):
        time.sleep(100)
     result = chat_model.predict(content + "에 대한 단편소설을 써줘")
-    
     st.write(result)
 
